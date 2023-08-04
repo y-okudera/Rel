cc2b600afdd3b4abe8378c661031e92d9e45f736
yuoku <appledev.yuoku@gmail.com>
2023-08-04 21:30:06 +0900

    Initial Commit

---
+++ Rel/RelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelApp.swift
+//  Rel
+//
+//  Created by Yuki Okudera on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct RelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
